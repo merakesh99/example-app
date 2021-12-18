a1c8028c3d6884e018a9292f1dfa6a6a178867e0
Rakesh Sadhukhan <rakeshvirat99@gmail.com>
2021-12-18 17:00:21 +0530

    first commit

---
+++ database/migrations/2021_12_18_110347_create_records_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRecordsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('records', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->integer('phone_no');
+            $table->string('city');
+            $table->integer('pincode');
+            $table->date('date');
+            $table->string('company_name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('records');
+    }
+}
