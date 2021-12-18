e71d002ef76a4d9dd7b571f18b902e8cf3e22f73
Rakesh Sadhukhan <rakeshvirat99@gmail.com>
2021-12-18 18:37:26 +0530

    changes

--- app/Models/Record.php
+++ app/Models/Record.php
@@ -8,4 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Record extends Model
 {
     use HasFactory;
+    // protected $fillable =['name','email','phone_no','city','pincode','date',company_name]
 }
